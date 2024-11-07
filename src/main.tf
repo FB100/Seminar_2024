abe8682fab7a177847ffbed23527f222c81140bf
Fabib <bo.fabian@protonmail.com>
2024-11-07 15:55:23 +0100

    4

--- src/main.tf
+++ src/main.tf
@@ -8,56 +8,37 @@ terraform {
 }
 
 provider "docker" {
-  // for Windows
-  // host = "npipe:////.//pipe//docker_engine"
 }
 
-variable "amount_docker_containers" {
+variable "amount" {
   description = "Number of Docker Containers"
   type        = number
   default     = 3
 }
 
-resource "docker_image" "ubuntu" {
-  name         = "ubuntu:precise"
+resource "docker_image" "nginx" {
+  name = "nginx"
+  keep_locally = false
 }
 
-resource "docker_container" "ubuntu" {
-  image = docker_image.ubuntu.image_id
-  count = 2
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  count = var.amount
   name  = "web-${count.index}"
   must_run = true
-
+  privileged = true
+  ports {
+    internal = 80
+    external = 8000 + count.index
+  }
   ports {
     internal = 22
-    external = 2200 + count.index
+    external = 9000 + count.index
   }
 
-  command = [
-    "sh", "-c",
-    "apt-get update && apt-get install -y openssh-server"
-  ]
-
-  connection {
-    type     = "ssh"
-    host     = self.network_data[0].ip_address
-    port     = 2200 + count.index
-    user     = "root"
-    password = "password"
-  }
-
-  provisioner "local-exec" {
-    command = "sleep 10"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Container is set up with SSH!'"
-    ]
-  }
 
 }
 
 output "IPAddr" {
-  value = [for container in docker_container.ubuntu : container.network_data[0].ip_address]
+  value = [for container in docker_container.nginx : container.network_data[0].ip_address]
 }
