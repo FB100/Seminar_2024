880508fb397a48fb2919b82fa488142e043f1e49
Fabib <bo.fabian@protonmail.com>
2024-11-09 16:07:05 +0100

    working ssh

--- src/main.tf
+++ src/main.tf
@@ -4,11 +4,14 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 3.0.2"
     }
+    ssh = {
+      source  = "loafoe/ssh"
+      version = "2.7.0"
+    }
   }
 }
 
-provider "docker" {
-}
+provider "docker" {}
 
 variable "amount" {
   description = "Number of Docker Containers"
@@ -16,29 +19,56 @@ variable "amount" {
   default     = 3
 }
 
-resource "docker_image" "nginx" {
-  name = "nginx"
-  keep_locally = false
+resource "docker_image" "ubuntu" {
+  name = "ubuntu"
+  build {
+    context = "."
+  }
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  count = var.amount
-  name  = "web-${count.index}"
-  must_run = true
+resource "docker_container" "ubuntu" {
+  image = docker_image.ubuntu.image_id
+  //count      = var.amount
+  name = "web"//"web-${count.index}"
+  must_run   = true
   privileged = true
   ports {
     internal = 80
-    external = 8000 + count.index
+    external = 8000 //+ count.index
   }
   ports {
     internal = 22
-    external = 9000 + count.index
+    external = 7000
+  }
+}
+/*
+
+resource "ssh_resource" "init" {
+  host  = docker_container.nginx
+  when  = "create"
+  agent = true
+  # An ssh-agent with your SSH private keys should be running
+  # Use 'private_key' to set the SSH key otherwise
+
+  # Try to complete in at most 15 minutes and wait 5 seconds between retries
+  timeout     = "15m"
+  retry_delay = "5s"
+
+  file {
+    content     = "echo Hello world"
+    destination = "/tmp/hello.sh"
+    permissions = "0700"
   }
 
-
+  commands = [
+    "/tmp/hello.sh"
+  ]
 }
+*/
 
+
+/*
 output "IPAddr" {
   value = [for container in docker_container.nginx : container.network_data[0].ip_address]
 }
+*/
