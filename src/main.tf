39aa9dc83bc4199de4fedc278edae899704d0386
Fabib <bo.fabian@protonmail.com>
2024-11-11 16:02:51 +0100

    ansible not working

--- src/main.tf
+++ src/main.tf
@@ -28,47 +28,22 @@ resource "docker_image" "ubuntu" {
 
 resource "docker_container" "ubuntu" {
   image = docker_image.ubuntu.image_id
-  //count      = var.amount
-  name = "web"//"web-${count.index}"
+  count      = var.amount
+  name = "web-${count.index}"
   must_run   = true
   privileged = true
   ports {
     internal = 80
-    external = 8000 //+ count.index
+    external = 8000 + count.index
   }
   ports {
     internal = 22
-    external = 7000
-  }
-}
-/*
-
-resource "ssh_resource" "init" {
-  host  = docker_container.nginx
-  when  = "create"
-  agent = true
-  # An ssh-agent with your SSH private keys should be running
-  # Use 'private_key' to set the SSH key otherwise
-
-  # Try to complete in at most 15 minutes and wait 5 seconds between retries
-  timeout     = "15m"
-  retry_delay = "5s"
-
-  file {
-    content     = "echo Hello world"
-    destination = "/tmp/hello.sh"
-    permissions = "0700"
+    external = 7000 + count.index
   }
 
-  commands = [
-    "/tmp/hello.sh"
-  ]
+
 }
-*/
 
-
-/*
 output "IPAddr" {
-  value = [for container in docker_container.nginx : container.network_data[0].ip_address]
+  value = [for container in docker_container.ubuntu : container.network_data[0].ip_address]
 }
-*/
